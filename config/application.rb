ddf60fb0808cc07f90a995b9a393691f76bb16ca
Ben Hsieh <bqh5026@gmail.com>
2025-05-09 12:31:08 -0500

    Upgrade ruby version to 3.1.7 and rails version to 6.1.7.10

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+require 'logger'
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -9,7 +11,9 @@ Bundler.require(*Rails.groups)
 module Robinhood
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    # config.load_defaults 5.2
+    config.load_defaults 6.1
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
