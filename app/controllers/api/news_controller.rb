846aa36909583a242b97b433f9ade69e94d84cf4
Ben Hsieh <bqh5026@gmail.com>
2025-08-06 21:59:19 -0500

    Update deployment parameters

--- app/controllers/api/news_controller.rb
+++ app/controllers/api/news_controller.rb
@@ -2,17 +2,13 @@ require 'net/http'
 
 class Api::NewsController < ApplicationController
 
-
-def new
-
-    url = URI.parse('http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=eb142e3a8c1b4af9a849e5ee78714783')
-    # url = URI.parse('http://newsapi.org/v2/everything?q=bitcoint&apiKey=eb142e3a8c1b4af9a849e5ee78714783')
+  def new
+    url = URI.parse("http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=#{ENV['NEWS_API_KEY']}")
     req = Net::HTTP::Get.new(url.to_s)
     res = Net::HTTP.start(url.host, url.port) {|http|
-    http.request(req)
+      http.request(req)
     }
     render :json => res.body 
-
   end
 
 end
