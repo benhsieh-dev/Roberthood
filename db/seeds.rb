7eda7ac722e51e77a959635a14e0b04e5337cad2
Ben Hsieh <bqh5026@gmail.com>
2025-08-07 21:03:11 -0500

    Fix seeds file to match User model schema
    - Remove non-existent columns (email, day_of_birth, etc.)
    - Seeds now only sets username, password, first_name, last_name
    - This matches the actual User model structure after migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,8 @@
 # Create demo user for the application
 demo_user = User.find_or_create_by(username: "bqh5026") do |user|
   user.password = "password"
-  user.email = "demo@roberthood.com"
   user.first_name = "Demo"
   user.last_name = "User"
-  user.day_of_birth = 1
-  user.month_of_birth = 1
-  user.year_of_birth = 1990
 end
 
 puts "Demo user created/updated: #{demo_user.username}"
