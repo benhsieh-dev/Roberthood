ed84fd5d3a7c0c3fd17ba01b6e9510f8cdd6764c
Ben Hsieh <bqh5026@gmail.com>
2025-08-09 13:47:54 -0500

    Fix Firebase environment variables in webpack build
    Configure webpack to properly inject Firebase environment variables
    during build process to resolve auth/invalid-api-key errors.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+// Add environment variables to webpack
+environment.plugins.prepend('Environment', new webpack.EnvironmentPlugin([
+  'REACT_APP_FIREBASE_API_KEY',
+  'REACT_APP_FIREBASE_MESSAGING_SENDER_ID', 
+  'REACT_APP_FIREBASE_APP_ID',
+  'REACT_APP_FIREBASE_MEASUREMENT_ID'
+]))
 
 module.exports = environment
